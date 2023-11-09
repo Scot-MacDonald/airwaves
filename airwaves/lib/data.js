a5695a3368806fd01b74c7c944e79aa113e93e7a
Scot MacDonald <scotmacdonald@hotmail.com>
2023-11-09 10:26:19 +0100

    airwaves Header

---
+++ airwaves/lib/data.js
@@ -0,0 +1,59 @@
+export const volumes = [
+  {
+    slug: "time-is-away-the-colour-of-pomegranates",
+    title: "time is away the colour of pomegranates",
+    description:
+      "This episode is inspired by the visionary world of Armenian filmmaker Sergei Parajanov. It draws on his 1969 masterpiece Sayat Nova, also known as The Colour of Pomegranates, a dreamlike poetic reimaginig of the life of the title character, an 18th-century poet and troubadour. ",
+    cover: "/images/the-fellowship-of-the-ring.png",
+    books: [
+      {
+        ordinal: "Book I",
+        title: "The Ring Sets Out",
+      },
+      {
+        ordinal: "Book II",
+        title: "The Ring Goes South",
+      },
+    ],
+    color: "#242C31",
+  },
+  {
+    slug: "the-two-towers",
+    title: "The Two Towers",
+    description:
+      "The Two Towers is the second volume of J. R. R. Tolkien's epic adventure The Lord of the Rings. It is preceded by The Fellowship of the Ring and followed by The Return of the King.",
+    cover: "/images/the-two-towers.png",
+    books: [
+      {
+        ordinal: "Book III",
+        title: "The Treason of Isengard",
+      },
+      {
+        ordinal: "Book IV",
+        title: "The Ring Goes East",
+      },
+    ],
+    color: "#532724",
+  },
+  {
+    slug: "the-return-of-the-king",
+    title: "The Return of the King",
+    description:
+      "The Return of the King is the third and final volume of J. R. R. Tolkien's epic adventure The Lord of the Rings. It is preceded by The Fellowship of the Ring and The Two Towers.",
+    cover: "/images/the-return-of-the-king.png",
+    books: [
+      {
+        ordinal: "Book V",
+        title: "The War of the Ring",
+      },
+      {
+        ordinal: "Book VI",
+        title: "The End of the Third Age",
+      },
+    ],
+    color: "#202C2C",
+  },
+];
+
+export const introduction =
+  "The Lord of the Rings is an epic high-fantasy novel by English author and scholar J. R. R. Tolkien. Set in Middle-earth, intended to be Earth at some time in the distant past, the story began as a sequel to Tolkien's 1937 children's book The Hobbit, but eventually developed into a much larger work. Written in stages between 1937 and 1949, The Lord of the Rings is one of the best-selling books ever written, with over 150 million copies sold.";
